099f830fdfa4e773b5cdd24a6ec13654b347e4fe
Daniil Kalamin <56044849+dakalamin@users.noreply.github.com>
2022-03-09 04:14:12 +0300

    Further update /core

--- include/core/utils.h
+++ include/core/utils.h
@@ -23,42 +23,32 @@ using mask_mch  = uint64_t;  // whole board describing type (like i/o masks)
 using coord_mch = int_mch;   // board coordinate values
 using count_mch = uint16_t;  // integer counters type
 
-template <typename T> 
-struct minmax { T MIN; T MAX; };
-
 
 namespace Board
 {
-    #define _MIN_WIDTH  6
-    #define _MIN_HEIGHT 6
-    #define _MAX_WIDTH  26  // number of letters in english alphabet
-    #define _MAX_HEIGHT 99
-
-    const int_mch MIN_WIDTH  = _MIN_WIDTH;
-    const int_mch MIN_HEIGHT = _MIN_HEIGHT;
-    const int_mch MAX_WIDTH  = _MAX_WIDTH;
-    const int_mch MAX_HEIGHT = _MAX_HEIGHT;
+    #define _MIN_SIDE  6
+    #define _MAX_SIDE  26  // number of letters in english alphabet
 
     #ifdef BOARD_SIDE
         #if defined(BOARD_WIDTH) || defined(BOARD_HEIGHT)
             #error Eiter BOARD_SIDE or both BOARD_WIDTH & BOARD_HEIGHT can be defined, but not simulataniously
-        #elif BOARD_SIDE < min(_MIN_WIDTH, _MIN_HEIGHT)
-            #error Board is too small! Minimal size is 6x6
-        #elif BOARD_SIDE > min(_MAX_WIDTH, _MAX_HEIGHT)
-            #error Board is too big! Maximum size is 26x26
+        #elif BOARD_SIDE < _MIN_SIDE
+            #error BOARD_SIDE is too small! Must be in interval between 6 and 26
+        #elif BOARD_SIDE > _MAX_SIDE
+            #error BOARD_SIDE is too big! Must be in interval between 6 and 26
         #endif
 
         #define BOARD_WIDTH  BOARD_SIDE
         #define BOARD_HEIGHT BOARD_SIDE
     #elif defined(BOARD_WIDTH) && defined(BOARD_HEIGHT)
-        #if   BOARD_WIDTH  < MIN_WIDTH
+        #if   BOARD_WIDTH  < _MIN_SIDE
             #error BOARD_WIDTH is too small! Must be in interval between 6 and 26
-        #elif BOARD_WIDTH  > MAX_WIDTH
+        #elif BOARD_WIDTH  > _MAX_SIDE
             #error BOARD_WIDTH is too big! Must be in interval between 6 and 26
-        #elif BOARD_HEIGHT < MIN_HEIGHT
-            #error BOARD_HEIGHT is too small! Must be in interval between 6 and 99
-        #elif BOARD_HEIGHT > MAX_HEIGHT
-            #error BOARD_HEIGHT is too big! Must be in interval between 6 and 99
+        #elif BOARD_HEIGHT < _MIN_SIDE
+            #error BOARD_HEIGHT is too small! Must be in interval between 6 and 26
+        #elif BOARD_HEIGHT > _MAX_SIDE
+            #error BOARD_HEIGHT is too big! Must be in interval between 6 and 26
         #endif
     #else
         #error Eiter BOARD_SIDE or both BOARD_WIDTH & BOARD_HEIGHT must be defined
@@ -66,12 +56,10 @@ namespace Board
 
     const int_mch WIDTH  = BOARD_WIDTH;
     const int_mch HEIGHT = BOARD_HEIGHT;
-    const int_mch SIZE   = WIDTH*HEIGHT;
+    const coord_mch SIZE = WIDTH*HEIGHT;
 
-    #undef _MIN_WIDTH
-    #undef _MIN_HEIGHT
-    #undef _MAX_WIDTH
-    #undef _MAX_HEIGHT
+    #undef _MIN_SIDE
+    #undef _MAX_SIDE
     
     #undef BOARD_WIDTH
     #undef BOARD_HEIGHT
